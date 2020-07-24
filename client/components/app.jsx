933fa6d66f584379b98b92531b84fd954744d757
brandonktran <brandontran@berkeley.edu>
2020-07-24 00:12:06 +0000

    add item front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,9 +9,11 @@ export default class App extends React.Component {
     this.state = {
       message: null,
       isLoading: true,
-      view: { name: 'catalog', params: {} }
+      view: { name: 'catalog', params: {} },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   componentDidMount() {
@@ -20,25 +22,51 @@ export default class App extends React.Component {
       .then(data => this.setState({ message: data.message || data.error }))
       .catch(err => this.setState({ message: err.message }))
       .finally(() => this.setState({ isLoading: false }));
+
+    this.getCartItems();
   }
 
   setView(name, params) {
     this.setState({ view: { name: name, params: params } });
   }
 
+  getCartItems() {
+    fetch('/api/cart')
+      .then(res => res.json())
+      .then(data => this.setState({ cart: data }));
+  }
+
+  addToCart(product) {
+    fetch('api/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ productId: product.productId }) //
+    }).then(res => res.json())
+      .then(data => this.setState((prevState, props) => {
+        const newArray = [...prevState.cart];
+        newArray.push(product);
+        return (
+          {
+            cart: newArray
+          });
+      }));
+  }
+
   render() {
     if (this.state.view.name === 'catalog') {
       return (
         <>
-          <Header />
+          <Header cartItemCount={this.state.cart.length} />
           <ProductList setView={this.setView} />
         </>
       );
     } else {
       return (
         <>
-          <Header />
-          <ProductDetails params={this.state.view.params} setView={this.setView} />
+          <Header cartItemCount={this.state.cart.length} />
+          <ProductDetails addToCart={this.addToCart} params={this.state.view.params} setView={this.setView} />
         </>
       );
     }
