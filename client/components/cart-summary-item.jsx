0e3b47ebc478f180be536eef1ac3ec520052103b
brandonktran <brandontran@berkeley.edu>
2020-07-28 22:11:55 +0000

    added cartDeleteModal

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -18,7 +18,25 @@ export default class CartSummaryItem extends React.Component {
               <button href="#" className="btn incrementers next border" onClick={() => { this.props.addToCart(this.props.item, 1); }}>+</button>
             </div>
             <p>{this.props.item.shortDescription}</p>
-            <button type="button" className="btn btn-danger" onClick={() => { this.props.deleteAllFromCart(this.props.item.cartItemId); }}>Remove all from Cart</button>
+            <button type="button" className="btn btn-danger ml-2" data-toggle="modal" data-target={`#ModalDelete${this.props.item.cartItemId}`}>
+              Remove All
+            </button>
+            <div className="modal fade" id={`ModalDelete${this.props.item.cartItemId}`} tabIndex="-1" role="dialog" aria-labelledby="ModalDeleteCenterTitle" aria-hidden="true">
+              <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content">
+                  <div className="modal-body">
+                    Are you sure you want to remove these items from your cart?
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => { this.props.deleteAllFromCart(this.props.item.cartItemId); }}>Remove All From Cart</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
